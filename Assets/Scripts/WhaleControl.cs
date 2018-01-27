0151f067d808f9a2d6a03533915401c33e1fc109
Essi Jukkala <essi.jukkala@playmoregames.com>
2018-01-27 16:16:18 +0200

    whale random pairing script

--- Assets/Scripts/WhaleControl.cs
+++ Assets/Scripts/WhaleControl.cs
@@ -5,10 +5,38 @@ using UnityEngine;
 public class WhaleControl : MonoBehaviour {
 
 	public List<GameObject> whales;
+	public List<GameObject> couple1;
+	public List<GameObject> couple2;
+	public List<GameObject> couple3;
 	float force = 100f;
+
 
 	void Start () {
 		Physics.IgnoreLayerCollision(8,8);
 		whales = new List<GameObject>(GameObject.FindGameObjectsWithTag("Whale"));
+		List<GameObject> whalesTemp = new List<GameObject>(whales);
+
+		couple1 = new List<GameObject>();
+		couple1 = new List<GameObject>();
+		couple1 = new List<GameObject>();
+
+		for(int i=0; i<3; i++){
+			int idx = Random.Range(0,whalesTemp.Count);
+			couple1.Add(whalesTemp[idx]);
+			whalesTemp.RemoveAt(idx);
+		}
+		for(int i=0; i<3; i++){
+			int idx = Random.Range(0,whalesTemp.Count);
+			couple2.Add(whalesTemp[idx]);
+			whalesTemp.RemoveAt(idx);
+		}
+		for(int i=0; i<3; i++){
+			int idx = Random.Range(0,whalesTemp.Count);
+			couple3.Add(whalesTemp[idx]);
+			whalesTemp.RemoveAt(idx);
+		}
+
 	}
+
+
 }
