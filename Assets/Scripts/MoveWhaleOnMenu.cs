7cf26bcbf6b68ff45eba0e04780eed113df39635
Mikael Kauppinen <mikael.kauppinen@hiq.fi>
2018-01-27 21:54:08 +0200

    change whale spawning to a different script. modify whale movements. still lots of bugs

--- Assets/Scripts/MoveWhaleOnMenu.cs
+++ Assets/Scripts/MoveWhaleOnMenu.cs
@@ -6,20 +6,10 @@ using UnityEngine;
 public class MoveWhaleOnMenu : MonoBehaviour {
 
 	private Vector3 direction = Vector3.left;
-	private int valasCount = 0;
-	
-	public GameObject valas;
-	
+
 	// Use this for initialization
 	void Start () {
-		/* System.Random rnd = new System.Random();
-		
-		for (int i = 0; i < 6; ++i) {
-			float coordX = rnd.Next(-14,15) * 1.0F;
-			float coordY = rnd.Next(-8,9) * 1.0F;
-			
-			Instantiate(valas, new Vector3(coordX, coordY, 0), Quaternion.identity);
-		} */
+
 	}
 	
 	// Update is called once per frame
@@ -28,17 +18,14 @@ public class MoveWhaleOnMenu : MonoBehaviour {
 		// transform.Rotate(0,2*Time.deltaTime, 0);
 		
 		if (Math.Abs(transform.position.x) > 14) {
-			transform.localScale = new Vector3(transform.localScale.x *-1, transform.localScale.y, transform.localScale.z);
-			direction = direction * -1;
+			Flip();
 			// transform.Translate(direction * Time.deltaTime);
 			// transform.Rotate(new Vector3(0,180,0));
 		}
 	}
-	
-	/* void OnCollisionEnter(Collision collision) 
-	{
-		Debug.Log("törmäys!\n");
+
+	public void Flip() {
 		transform.localScale = new Vector3(transform.localScale.x *-1, transform.localScale.y, transform.localScale.z);
 		direction = direction * -1;
-	} */
+	}
 }
---
+++ Assets/SpawnWhales.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnWhales : MonoBehaviour {
+
+	public GameObject valas;
+
+	// Use this for initialization
+	void Start () {
+		System.Random rnd = new System.Random();
+		
+		for (int i = 0; i < 4; i++)
+		{
+			float coordX = rnd.Next(-14,15) * 1.0F;
+			float coordY = rnd.Next(-4,4) * 1.0F;
+
+			var uusiValas = Instantiate(valas, new Vector3(coordX, coordY, 0), Quaternion.identity);
+
+			if (i%2 == 0) {
+				uusiValas.GetComponent<MoveWhaleOnMenu>().Flip();
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
