6691007a611bb0d52e1c974989dd52fd00a37e51
Mikael Kauppinen <mikael.kauppinen@hiq.fi>
2018-01-27 16:58:52 +0200

    add menu scene. change build settings

---
+++ Assets/Scripts/button_control.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class button_control : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void LoadByIndex(int sceneIndex)
+	{
+		SceneManager.LoadScene(sceneIndex);
+	}
+	
+	public void Quit() 
+	{	
+#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+#else
+		Application.Quit();
+#endif	
+	}
+}
