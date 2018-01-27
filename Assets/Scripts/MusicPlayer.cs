7fb357d511a138f1f29e600adae0703a32eebe01
Essi Jukkala <essi.jukkala@playmoregames.com>
2018-01-27 21:27:27 +0200

    added music player

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+	public AudioClip intro;
+	public AudioClip loop;
+	public AudioSource player;
+
+	void Start(){
+		player.clip = intro;
+		player.Play();
+	}
+
+	void Update(){
+		if(!player.isPlaying){
+			player.clip = loop;
+			player.loop = true;
+			player.Play();
+		}
+	}
+}
